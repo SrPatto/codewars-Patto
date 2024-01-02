cf9cdf35b9390a7037a787331bc361118f9475dc
SrPatto <ladrianrv@hotmail.com>
2024-01-02 09:44:19 -0600

    Returning Strings

---
+++ 8kyu/Returning Strings.cpp
@@ -0,0 +1,13 @@
+// Description:
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+//My solution
+#include <string>
+
+std::string greet(const std::string& n){
+  //your code here
+  return "Hello, " + n + " how are you doing today?";
+}
