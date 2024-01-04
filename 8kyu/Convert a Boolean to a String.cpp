f36afd4bdfa1a815ed4e571f0f740214c1f2451e
SrPatto <ladrianrv@hotmail.com>
2024-01-04 09:27:56 -0600

    Convert a Boolean to a String

---
+++ 8kyu/Convert a Boolean to a String.cpp
@@ -0,0 +1,12 @@
+// Description:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//My solution
+#include <string>
+
+std::string boolean_to_string(bool b){
+  return (b ? "true" : "false");
+}
